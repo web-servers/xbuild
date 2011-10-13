8b3546bf450779765e2e9e8d8f22ffd94155fa11
Mladen Turk <mturk@redhat.com>
2011-10-13 15:53:02 +0000

    Allow options for xbuild

--- tools/mkbrew.sh
+++ tools/mkbrew.sh
@@ -2,21 +2,28 @@
 #
 TAG=jb-ep-5-win-candidate
 VM=jboss-natives-20110516-1
+CMD=win-build
 for o
 do
     case $o in
         --tag=* )
-            TAG="`sed 's/.*=//'`"            
+            TAG="`sed 's/.*=//'`"
+            shift
         ;;
         --vm=* )
             VM="`sed 's/.*=//'`"
+            shift
         ;;
-        * )
+        --*    )
+            CMD="$CMD $o"
+            shift
+        ;;
+        *      )
           echo "Unknown option $o"
           exit 1
         ;;
     esac
 done
 HEAD=`svn info http://anonsvn.jboss.org/repos/xbuild/trunk | grep Revision: | sed 's/.*: //'`
-echo "$TAG svn+http://anonsvn.jboss.org/repos/xbuild?trunk#$HEAD $VM"
-brew win-build $TAG svn+http://anonsvn.jboss.org/repos/xbuild?trunk#$HEAD $VM
+echo "$CMD $TAG svn+http://anonsvn.jboss.org/repos/xbuild?trunk#$HEAD $VM"
+brew $CMD $TAG svn+http://anonsvn.jboss.org/repos/xbuild?trunk#$HEAD $VM
