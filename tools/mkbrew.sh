5eeaa4a0484f47ebeee8be0f42574b2ed2834070
Mladen Turk <mturk@redhat.com>
2011-10-13 15:36:32 +0000

    Add brew win-build helper. Note that win-build will have to go away when we'll support Solaris

---
+++ tools/mkbrew.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+TAG=jb-ep-5-win-candidate
+VM=jboss-natives-20110516-1
+for o
+do
+    case $o in
+        --tag=* )
+            TAG="`sed 's/.*=//'`"            
+        ;;
+        --vm=* )
+            VM="`sed 's/.*=//'`"
+        ;;
+        * )
+          echo "Unknown option $o"
+          exit 1
+        ;;
+    esac
+done
+HEAD=`svn info | grep Revision: | sed 's/.*: //'`
+
+echo "$TAG svn+http://anonsvn.jboss.org/repos/xbuild?trunk#$HEAD $VM"
+brew win-build $TAG svn+http://anonsvn.jboss.org/repos/xbuild?trunk#$HEAD $VM
