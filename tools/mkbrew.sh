d9070004b3e96700a42a1206048929292e8a3075
Mladen Turk <mturk@redhat.com>
2014-03-24 10:12:27 +0000

    Fix typo

--- tools/mkbrew.sh
+++ tools/mkbrew.sh
@@ -24,6 +24,6 @@ do
         ;;
     esac
 done
-HEAD=`svn info http://anonsvn.jboss.org/repos/xbuild/branches/eap6.2 | grep Revision: | sed 's/.*: //'`
-echo "$CMD $TARGET svn+http://anonsvn.jboss.org/repos/xbuild?branches/eap6.2#$HEAD $VM"
-brew $CMD $TARGET svn+http://anonsvn.jboss.org/repos/xbuild?branches/eap6.2#$HEAD $VM
+HEAD=`svn info http://anonsvn.jboss.org/repos/xbuild/branches/eap-6.2 | grep Revision: | sed 's/.*: //'`
+echo "$CMD $TARGET svn+http://anonsvn.jboss.org/repos/xbuild?branches/eap-6.2#$HEAD $VM"
+brew $CMD $TARGET svn+http://anonsvn.jboss.org/repos/xbuild?branches/eap-6.2#$HEAD $VM
